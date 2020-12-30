d4bf80f42634b58e015d99ebc2a120f536746779
Robert Mueller <37350628+robmul05@users.noreply.github.com>
2020-12-30 13:44:20 -0500

    Update main.py
    Better GUI

--- clockgui/main.py
+++ clockgui/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import font as tkFont
 import datetime
 import platform
 try:
@@ -9,17 +10,36 @@ except:
 import math
 window = Tk()
 window.title("Time Clock")
-window.geometry('500x250')
-stopwatch_counter_num = 66600
-stopwatch_running = False
+window.geometry('900x500')
+
+time_font = tkFont.Font(size=12,weight='bold')
+current_time = datetime.datetime.now().strftime("%m-%d-%Y %H:%M %p")
 
 def time_in(time):
-        print(time)
+	time_in_button.config(bg='green',state='disabled')
+	print(time)
+	if time_in_button['state'] == 'disabled':
+		time_in_button.after(1500,refresh_button_in)
 
+def time_out(time):
+	time_out_button.config(bg='red',state='disabled')
+	print(time)
+	if time_out_button['state'] == 'disabled':
+		time_out_button.after(1500,refresh_button_out)
+
+def refresh_button_out():
+	time_out_button.config(bg='gray', state='normal')
+		
+
+def refresh_button_in():
+	time_in_button.config(bg='gray', state='normal')
+
+
+def name():
+	name_label.config(text='Insert Name')
 
 def clock():
-
-        date_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S/%p")
+        date_time = datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S/%p")
         date,time1 = date_time.split()
         time2,time3 = time1.split('/')
         hour,minutes,seconds =  time2.split(':')
@@ -31,61 +51,36 @@ def clock():
         date_label.config(text= date)
         time_label.after(1000, clock)
 
-def clockin():
-        emp1 = 'Melonie'
-        emp2 = 'Meade'
+def get_name():
+	names = description.get()
+	print(names)
 
 
-
-def stopwatch(work):
-         if work == 'start':
-                 global stopwatch_running
-                 stopwatch_running=True
-                 stopwatch_start.config(state='disabled')
-                 stopwatch_stop.config(state='enabled')
-                 stopwatch_reset.config(state='enabled')
-
-         elif work == 'stop':
-                 stopwatch_running=False
-                 stopwatch_start.config(state='enabled')
-                 stopwatch_stop.config(state='disabled')
-                 stopwatch_reset.config(state='enabled')
-         elif work == 'reset':
-                 global stopwatch_counter_num
-                 stopwatch_running=False
-                 stopwatch_counter_num=66600
-                 stopwatch_label.config(text='Stopwatch')
-                 stopwatch_start.config(state='enabled')
-                 stopwatch_stop.config(state='disabled')
-                 stopwatch_reset.config(state='disabled')
 
 tabs_control = ttk.Notebook(window)
 clock_tab = Frame(tabs_control)
 
-stopwatch_tab = Frame(tabs_control)
-
+name_label = Label(clock_tab,font='calibri 12',text='Insert Name')
+name_label.grid(column=1,row=3,pady=20)
+description = StringVar()
+name_entry = Entry(clock_tab,width=20,textvariable=description)
+name_entry.grid(row=3,column=2)
 
 tabs_control.add(clock_tab, text="Clock")
 
-tabs_control.add(stopwatch_tab, text='Stopwatch')
-
-
 tabs_control.pack(expand = 1, fill ="both")
 time_label = Label(clock_tab, font = 'calibri 40 bold', foreground = 'black')
-time_label.pack(anchor='center')
+time_label.grid(row=0,column=1,columnspan=2)
 date_label = Label(clock_tab, font = 'calibri 40 bold', foreground = 'black')
-date_label.pack(anchor='s')
+date_label.grid(row=1, column=1,columnspan=2)
 
-stopwatch_label = Label(stopwatch_tab, font='calibri 40 bold', text='Stopwatch')
-stopwatch_label.pack(anchor='center')
-stopwatch_start = Button(stopwatch_tab, text='Start', command=lambda:stopwatch('start'))
-stopwatch_start.pack(anchor='center')
-stopwatch_stop = Button(stopwatch_tab, text='Stop', state='disabled',command=lambda:stopwatch('stop'))
-stopwatch_stop.pack(anchor='center')
-stopwatch_reset = Button(stopwatch_tab, text='Reset', state='disabled', command=lambda:stopwatch('reset'))
-stopwatch_reset.pack(anchor='center')
-time_in_button = Button(clock_tab,height=75, width=100, command=lambda:time_in(12))
-time_in_button.pack(anchor='s')
+
+time_in_button = Button(clock_tab,height=7, width=20, text='Clock In', font=time_font, bg='gray', command=lambda:[time_in(current_time),get_name()])
+time_in_button.grid(row=4, padx=50,pady=50)
+
+time_out_button = Button(clock_tab, height=7,width=20, text='Clock Out', font=time_font,bg='gray',command=lambda:[time_out(current_time),get_name()])
+time_out_button.grid(row=4, column=3)
+
 clock()
 
 window.mainloop()
